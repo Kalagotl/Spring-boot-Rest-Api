85d83880fd896e5d9ccd5ff889b315eb5762e69c
Dev1 <shekhardpd@gmail.com>
2024-06-21 21:04:08 +0530

    Normal Check

--- spring-boot-Rest-Api/src/main/java/com/lnp/restapi/SpringBootRestApiApplication.java
+++ spring-boot-Rest-Api/src/main/java/com/lnp/restapi/SpringBootRestApiApplication.java
@@ -5,7 +5,7 @@ import org.apache.logging.log4j.Logger;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication // used 
 public class SpringBootRestApiApplication {
 
     private static final Logger logger = LogManager.getLogger(SpringBootRestApiApplication.class);
